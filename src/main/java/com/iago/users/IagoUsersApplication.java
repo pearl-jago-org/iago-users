688c1eb2d865a2ba9d280e5e540821c19873d1b5
pearljago <pearl.jago@gmail.com>
2026-06-10 11:08:20 +0200

    changed endpoints

--- src/main/java/com/iago/users/IagoUsersApplication.java
+++ src/main/java/com/iago/users/IagoUsersApplication.java
@@ -1,11 +1,12 @@
-package com.iago.app;
+package com.iago.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class IagoUsersApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(IagoUsersApplication.class, args);
     }
 }
---
+++ src/main/java/com/iago/users/controller/UsersController.java
@@ -0,0 +1,13 @@
+package com.iago.users.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersController {
+
+    @GetMapping("/users")
+    public String users() {
+        return "iago-users v1.0.0";
+    }
+}
